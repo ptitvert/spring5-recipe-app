c8e0769722649480540a9075a83a56c6d21e7530
Alessandro Perucchi <alessandro@perucchi.org>
2018-03-18 11:39:04 +0100

    Initial Commit

---
+++ src/main/java/org/perucchi/springs/course/recipe/RecipeProjectApplication.java
@@ -0,0 +1,12 @@
+package org.perucchi.springs.course.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeProjectApplication.class, args);
+	}
+}
